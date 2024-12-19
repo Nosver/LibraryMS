e3aed7b4e8b8c78d22ac5a3e579a2a85c095a01b
MasisOznigolyan <oznigolyan3@gmail.com>
2024-12-19 13:39:59 +0300

    Update header.php

--- MS/header.php
+++ MS/header.php
@@ -6,50 +6,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-gray-100 p-6">
-    <header class="bg-white-600 text-black p-4 w-full rounded-lg shadow-lg">
-        <div class="container mx-auto flex justify-between items-center">
-            <div>
-                <a href="https://github.com/Nosver">
-                    <img src="/library/images/nosver.png" alt="Logo" class="h-[60px]">
-                </a>
-            </div>
-            <nav>
-                <ul class="flex space-x-6 text-sm font-medium">
-
-                    <li>
-                        <?php 
-                        if (isset($_SESSION['user']) && $_SESSION['user']['role'] === 'CUSTOMER') { 
-                        ?>
-                            <a href="bookCatalogue.php" class="hover:underline">Catalogue</a>
-                        <?php 
-                        } elseif (isset($_SESSION['user']) && $_SESSION['user']['role'] === 'STAFF') { 
-                        ?>
-                            <a href="staff.php" class="hover:underline">Dashboard</a>
-                        <?php 
-                        } 
-                        ?>
-                    </li>
-
-                    <li>
-                        <a href="<?php echo isset($_SESSION['user']) && $_SESSION['user']['role'] === 'CUSTOMER' 
-                                        ? 'userTransactions.php' 
-                                        : 'staffTransactions.php'; ?>" 
-                        class="hover:underline">Transactions</a>
-                    </li>
-
-                    <li><a href="myprofile.php" class="hover:underline">Profile</a></li>
-                   <?php if (isset($_SESSION['user']))
-                        echo "<li><a href='logout.php' class='hover:underline text-red-500'>Logout</a></li>";
-                        else {
-                           echo "<li><a href='login.php' class='hover:underline text-red-500'>Login</a></li>";
-                        }?>
-                </ul>
-            </nav>
+<body class="bg-gray-100 p-6 flex flex-col min-h-screen">
+<header class="fixed top-0 left-0 w-full bg-white text-black p-4 rounded-lg shadow-lg z-50">
+    <div class="container mx-auto flex justify-between items-center">
+        <div>
+            <a href="https://github.com/Nosver">
+                <img src="/library/images/nosver.png" alt="Logo" class="h-[60px]">
+            </a>
         </div>
-    </header>
+        <nav>
+            <ul class="flex space-x-6 text-sm font-medium">
+                <li>
+                    <?php
+                    if (isset($_SESSION['user'])) {
+                        if ($_SESSION['user']['role'] === 'CUSTOMER') {
+                            echo '<a href="bookCatalogue.php" class="hover:underline">Catalogue</a>';
+                        } elseif ($_SESSION['user']['role'] === 'STAFF') {
+                            echo '<a href="staff.php" class="hover:underline">Dashboard</a>';
+                        }
+                    } else {
+                        echo '<a href="bookCatalogue.php" class="hover:underline">Catalogue</a>';
+                    }
+                    ?>
+                </li>
+                <?php if (isset($_SESSION['user']) && $_SESSION['user']['role'] === 'CUSTOMER') { ?>
+                    <li>
+                        <a href="userTransactions.php" class="hover:underline">Transactions</a>
+                    </li>
+                    <li>
+                        <a href="myprofile.php" class="hover:underline">Profile</a>
+                    </li>
+                    <li>
+                        <a href="logout.php" class="hover:underline text-red-500">Logout</a>
+                    </li>
+                <?php } elseif (isset($_SESSION['user']) && $_SESSION['user']['role'] === 'STAFF') { ?>
+                    <li>
+                        <a href="staffTransactions.php" class="hover:underline">Transactions</a>
+                    </li>
 
-    <main class="mb-[20px]">
-    </main>
+                    <li>
+                        <a href="myprofile.php" class="hover:underline">Profile</a>
+                    </li>
+
+                    <li>
+                        <a href="logout.php" class="hover:underline text-red-500">Logout</a>
+                    </li>
+                <?php } elseif (!isset($_SESSION['user'])) { ?>
+                    <li>
+                        <a href="login.php" class="hover:underline text-red-500">Login</a>
+                    </li>
+                <?php } ?>
+            </ul>
+
+        </nav>
+    </div>
+</header>
+
+<main class=" mt-[90px]">
+</main>
+
 </body>
 </html>
